12d440593fded87f5eb4ea2adab3bc75e83e4b8d
Will Eccles <willeccles860+github@gmail.com>
2016-10-17 14:44:59 -0400

    Now working.

--- oodlebot.js
+++ oodlebot.js
@@ -4,6 +4,7 @@ const discord = require('discord.js'),
 const client = new discord.Client();
 
 var token = "";
+var clientID = "";
 
 // load the config
 try {
@@ -15,17 +16,25 @@ try {
 		console.error("Please set your token in config.json.");
 		process.exit(1);
 	}
+	if (settings.clientID) {
+		clientID = settings.clientID;
+	} else {
+		console.error("Please set your clientID in config.json.");
+		process.exit(2);
+	}
 } catch (err) {
-	console.error("Error reading config.json.\nYou may need to create this file.\nMake it say:\n   {\"token\":\"yourtokenhere\"}");
+	console.error("Error reading config.json.\nYou may need to create this file.\nMake it say:\n   {\"token\":\"yourtokenhere\",\"clientID\":\"clientID\"}");
 }
 
 client.on('ready', () => {
 	console.log("Client ready.");
+	console.info(`Invite link:\nhttps://discordapp.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=${7168}`);
+	// the 7168 there is the permissions, and that one basically just means the bot can read messages and post them.
 });
 
 client.on('message', message => {
 	var reply = false;
-	var pattern = /[a-zA-Z]/g;
+	var pattern = /[aeiou]/ig;
 	var replacement = "oodle";
 	var command = "!oodle";
 	if (/^!oodle\s+/i.test(message.content)) {
@@ -49,11 +58,14 @@ client.on('message', message => {
 	}
 
 	if (reply) {
-		var msg = message.content.replace(new RegExp("^"+command+"\s+", "i"), "").replace(pattern, replacement);
+		var commandregex = new RegExp("^" + command, "i");
+		var msg = message.content.replace(commandregex, "");
+		console.log(msg);
+		msg = msg.replace(pattern, replacement);
 		if (!/oodle/i.test(msg))
 			message.reply("Nothing there to oodle. (!help for more info)");
 		else
-			message.reply(msg);
+			message.reply(msg.trim());
 	}
 });
 
