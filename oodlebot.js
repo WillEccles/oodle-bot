5283d15cef714ce9d469bfb6c2ec730d8a03715e
Will Eccles <willeccles860+github@gmail.com>
2016-10-31 18:50:59 -0400

    Added oodletts command.

--- oodlebot.js
+++ oodlebot.js
@@ -26,13 +26,26 @@ try {
 	console.error("Error reading config.json.\nYou may need to create this file.\nMake it say:\n   {\"token\":\"yourtokenhere\",\"clientID\":\"clientID\"}");
 }
 
+// c = channel
+// p = permission (i.e. "SEND_MESSAGES")
+// u = user (if not the client user)
+var hasPermission(c, p, u = client.user) {
+	if (c.permissionsFor(u).hasPermission(p))
+		return true;
+	else return false;
+}
+
 client.on('ready', () => {
 	console.log("Client ready.");
 	//console.info(`Invite link:\nhttps://discordapp.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=${7168}`);
 	// the 7168 there is the permissions, and that one basically just means the bot can read messages and post them.
 });
 
-client.on('message', message => {
+client.on('message', (message) => {
+	if (!hasPermission(message.channel, "SEND_MESSAGES")) {
+		message.author.sendMessage(":warning: I don't have permission to send messages in that channel.");
+		return;
+	}
 	var reply = false;
 	var pattern = /[aeiou]/ig;
 	var replacement = "oodle";
@@ -52,18 +65,16 @@ client.on('message', message => {
 		replace = "Oodle";
 		reply = true;
 	}
-	/*else if (/^!oodleauto\s+(.*)?/i.test(message.content)) {
-		// this one is more complicated, and does not use the reply function below
-		reply = false;
-		var msg = message.content.replace(/^!oodleauto/i, "");
-		msg = msg.replace(/[aeiou]/g, "oodle")
-				.replace(/[AEIOU](?=[a-z])/g, "Oodle")
-				.replace(/[AEIOU](?!=[a-z])/g, "OODLE");
-		if (!/oodle/i.test(msg)) {
-			message.channel.sendMessage(":warning: Nothing there to oodle. (!oodlehelp for more info)");
+	else if (/^!oodletts\s+(.*)?/i.test(message.content)) {
+		var msg = message.content.trim().replace(/^!oodletts/i, "");
+		if (!hasPermission(message.channel, "SEND_TTS_MESSAGES")) {
+			message.reply(":warning: I don't have permission to TTS here.");
+		} else if (!hasPermission(message.channel, "SEND_TTS_MESSAGES", message.author)) {
+			message.reply(":warning: You don't have permission to TTS here.");
 		} else
-			message.channel.sendMessage(msg.trim());
-	}*/
+			message.channel.sendTTSMessage(msg.toLower().replace(/[aeiou]/ig, "oodle"));
+	}
+
 	// !delmessages -[fq] <number>
 	else if (/^!del(messages)?\s+(-[fq]{1,2}\s+)*\d+(\s+(-[fq]{1,2}\s*)*)?/i.test(message.content)) {
 		var force = false;
@@ -78,14 +89,14 @@ client.on('message', message => {
 		}
 
 		// first, find out the if bot has the ability to delete messages in the first place
-		if (message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+		if (hasPermission(message.channel, "MANAGE_MESSAGES", message.author)) {
 			// the user has the permission, so now we can delete the messages
 			var num = parseInt(/\d+/.exec(message.content));
 			if (num >= 100 || num < 2) {
 				// regect any more than 500 messages
 				message.channel.sendMessage(`:warning: I can only delete [2, 100) messages. ${num} is ${num>=100?"too many":"too few"}.`);
 			} else {
-				if (message.channel.permissionsFor(client.user).hasPermission("MANAGE_MESSAGES")) {
+				if (hasPermission(message.channel, "MANAGE_MESSAGES")) {
 					if (!quiet) message.channel.sendMessage(`Removing ${num} messages from this channel${force?" (including pins)":" (excluding pins)"}...`)
 						.then(msg => {
 							// add 1 to num to account for !delmessages <number>
@@ -107,8 +118,8 @@ client.on('message', message => {
 	}
 
 	else if (/^!oodlehelp/i.test(message.content)) {
-		message.author.sendMessage("Here are the things I can do for you:\n```\n!oodle <message>\n  replaces every vowel in <message> with 'oodle'\n!oodlecaps <MESSAGE>\n  replaces every vowel in <MESSAGE> with 'OODLE'\n!oodletitle <Message>\n  replaces every vowel in <Message> with 'Oodle'\n!oodleinvite\n  messages you the invite link for the bot\n!delmessages <number> or !del <number>\n  deletes <number> messages. also deletes your !del or !delnum message. <number> must be in the range [2, 100). user running the command must have the \"manage messages\" permission. any pinned messages found will be kept. if run with -f, will delete pinned messages. if run with -q, will not say anything.\n```");
-		if (message.channel.permissionsFor(client.user).hasPermission("MANAGE_MESSAGES")) {
+		message.author.sendMessage("Here are the things I can do for you:\n```\n!oodle <message>\n  replaces every vowel in <message> with 'oodle'\n!oodlecaps <MESSAGE>\n  replaces every vowel in <MESSAGE> with 'OODLE'\n!oodletitle <Message>\n  replaces every vowel in <Message> with 'Oodle'\n!oodleinvite\n  messages you the invite link for the bot\n!delmessages <number> or !del <number>\n  deletes <number> messages. also deletes your !del or !delnum message. <number> must be in the range [2, 100). user running the command must have the \"manage messages\" permission. any pinned messages found will be kept. if run with -f, will delete pinned messages. if run with -q, will not say anything.\n!oodletts <message>\n  replaces every vowel in the message with 'oodle', lowercases the message, and then TTSs the message.\n```");
+		if (hasPermission(message.channel, "MANAGE_MESSAGES")) {
 			message.delete();
 		}
 	}
